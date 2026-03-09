74e9f5a48b3ea2b7ab31e72fa2e16a24bd0092c4
Vyom <vyomchaturvedi1301@gmail.com>
2026-03-09 14:50:24 +0530

    Update PalindromeCheckerApp.java

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,15 +1,5 @@
 public class PalindromeCheckerApp {
-
-    public static void main(String[] args) {
-
-        uc1();
-        // uc2();
-        // uc3();
-        // uc4();
-        // uc5();
-        // uc6();
-    }
-
+    
     public static void uc1(){
         System.out.println("Welcome to Palindrome Checker App");
         System.out.println("Version 1.0");
